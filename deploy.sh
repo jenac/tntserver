5a061b1e630604fa46d0daf5d52eb751ee72083c
Lihe Chen <lihe.chen@gmail.com>
2016-12-28 13:46:26 -0600

    fix bash error

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-
     git config --global user.name "Travis CI"
     git config --global user.email "lihe.chen@gmail.com"
     git remote set-url origin https://jenac:$GITHUB_API_KEY@github.com/tnt-develop-team/tntserver.git
-    if ["$TRAVIS_BRANCH" == "master"]; then
+    if [ "$TRAVIS_BRANCH" == "master" ]; then
         ./gradlew tagReleaseVersion
         ./gradlew increaseBuildNumber
         git commit -am "increase build version [ci skip]"
