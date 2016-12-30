3e717f8d9e8851133efd68e5ee21760c6ca5f7ca
Lihe Chen <lihe.chen@gmail.com>
2016-12-29 20:21:13 -0600

    echo version

--- publish.sh
+++ publish.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
+echo $TRAVIS_BRANCH
 curl -k https://$DEPLOY_JENKINS_USER:$DEPLOY_JENKINS_PASS@52.165.30.58:8443/job/tntserver_deploy/buildWithParameters?token=$DEPLOY_JENKINS_TOKEN&VERSION=$TRAVIS_BRANCH
 sleep 5
