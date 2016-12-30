16d93fadd2c58058d4d6e53be86c0796a9a022b8
Lihe Chen <lihe.chen@gmail.com>
2016-12-30 11:26:01 -0600

    swith param

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 echo 'current branch'
 echo $TRAVIS_BRANCH
-curl -k https://$DEPLOY_JENKINS_USER:$DEPLOY_JENKINS_PASS@52.165.30.58:8443/job/tntserver_deploy/buildWithParameters?token=$DEPLOY_JENKINS_TOKEN&VERSION=$TRAVIS_BRANCH
+curl -k https://$DEPLOY_JENKINS_USER:$DEPLOY_JENKINS_PASS@52.165.30.58:8443/job/tntserver_deploy/buildWithParameters?VERSION=$TRAVIS_BRANCH&token=$DEPLOY_JENKINS_TOKEN
 sleep 5
