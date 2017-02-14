1990807ecb80171444ad1eb60ba8b72a501325ba
Lihe Chen <lihe.chen@gmail.com>
2017-02-13 22:12:03 -0600

    somehow lombok does not work with neo4j.

--- src/main/java/com/mnit/tnt/repository/UserRepository.java
+++ src/main/java/com/mnit/tnt/repository/UserRepository.java
@@ -8,7 +8,9 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
  * Created by lihe on 2/12/17.
  */
 
-@RepositoryRestResource(collectionResourceRel = "users", path = "users")
+@RepositoryRestResource(collectionResourceRel = "user", path = "user")
 public interface UserRepository extends PagingAndSortingRepository<User, Long>{
 
 }
+
+//curl -i -X POST -H "Content-Type:application/json" -d '{  "userName": "someone", "password": "somepass", "firstName" : "Frodo1",  "lastName" : "Baggins1", "email": "s@a.com", "stormPathHref": "https://stormpath", "valid": true, "dateCreated": null, "dateUpdated": null }' http://localhost:8080/users
