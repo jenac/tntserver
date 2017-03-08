85f62a8efc5e952c7fde750d52d4c0699d5bbc38
Lihe Chen <lihe.chen@gmail.com>
2017-03-07 20:58:08 -0600

    adding configuration to enable/disable stormpath

--- src/main/java/com/mnit/tnt/config/SpringSecurityWebAppConfig.java
+++ src/main/java/com/mnit/tnt/config/SpringSecurityWebAppConfig.java
@@ -1,5 +1,6 @@
 package com.mnit.tnt.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,14 +10,23 @@ import static com.stormpath.spring.config.StormpathWebSecurityConfigurer.stormpa
 /**
  * Created by lihe on 1/19/17.
  */
+
 @Configuration
 public class SpringSecurityWebAppConfig extends WebSecurityConfigurerAdapter {
+    @Value("${tntserver.stormpath.enabled}")
+// with default value set
+// @Value("${tntserver.stormpath.enabled: false}")
+    Boolean stormpathEnabled;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .apply(stormpath()).and()
-                .authorizeRequests()
-                .antMatchers("/").permitAll()
-                .antMatchers("/public/*").permitAll();
+        if (stormpathEnabled) {
+            http.apply(stormpath()).and()
+                    .authorizeRequests()
+                    .antMatchers("/").permitAll()
+                    .antMatchers("/public/*").permitAll();
+        } else {
+            http.authorizeRequests().antMatchers("/**").permitAll();
+        }
     }
 }
